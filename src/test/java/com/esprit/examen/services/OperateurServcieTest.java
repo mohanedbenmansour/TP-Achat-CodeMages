b2f0eb13f1556ab97ca67b1cc00ece6c5bfbed36
ferielll <47891897+ferielll@users.noreply.github.com>
2022-11-10 11:45:43 +0100

    Update OperateurServcieTest.java

--- src/test/java/com/esprit/examen/services/OperateurServcieTest.java
+++ src/test/java/com/esprit/examen/services/OperateurServcieTest.java
@@ -4,8 +4,12 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
-import java.util.Set;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 
 import com.esprit.examen.entities.Facture;
 import com.esprit.examen.entities.Operateur;
@@ -52,6 +56,42 @@ public class OperateurServcieTest {
 		assertEquals(o1, os.addOperateur(o1)); 
 		System.out.println("add works !");
 	}
+	
+   @Test 
+    public void retrieveAllOperateursTest() {
+    	when(or.findAll()).thenReturn(Stream
+    			.of(o1,o2)
+    			.collect(Collectors.toList()));
+    	assertEquals(2,os.retrieveAllOperateurs().size());
+    	System.out.println("Retrieve operators works !");
+    }
+    
+   
+    
+    @Test
+    public void DeleteOperateurTest() {
+    	or.save(o1);
+    	os.deleteOperateur(o1.getIdOperateur());
+    	verify(or, times(1)).deleteById(o1.getIdOperateur());
+    	System.out.println("Delete works !");
+    	
+    }
+
+    
+    @Test 
+    public void UpdateOperateurTest() {
+    	when(or.save(o1)).thenReturn(o1);
+    	assertNotNull(o1);
+    	assertEquals(o1, os.updateOperateur(o1));
+    	System.out.println("Update works !");
+    }
+    
+    @Test
+    public void retrieveOperateurTest() {
+    	when(or.findById(o1.getIdOperateur())).thenReturn(Optional.of(o1));
+    	assertEquals(o1, os.retrieveOperateur(o1.getIdOperateur()));
+    	System.out.println("Retrieve operator works !");
+    }
     
     
 }
