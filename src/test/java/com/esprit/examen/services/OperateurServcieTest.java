2f54423962b2d0cb1c6a5cb77dd351c8d8e21cf6
ferielll <47891897+ferielll@users.noreply.github.com>
2022-11-10 10:18:06 +0100

    Update OperateurServcieTest.java

--- src/test/java/com/esprit/examen/services/OperateurServcieTest.java
+++ src/test/java/com/esprit/examen/services/OperateurServcieTest.java
@@ -3,44 +3,32 @@ package com.esprit.examen.services;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
-import org.junit.jupiter.api.MethodOrderer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import com.esprit.examen.entities.Facture;
+import com.esprit.examen.entities.Operateur;
+import com.esprit.examen.repositories.OperateurRepository;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.junit.Test;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-
-import com.esprit.examen.entities.Produit;
-import com.esprit.examen.entities.Stock;
-import com.esprit.examen.repositories.FactureRepository;
-import com.esprit.examen.repositories.ProduitRepository;
-import com.esprit.examen.repositories.StockRepository;
-import com.esprit.examen.services.IProduitService;
-import org.junit.runner.RunWith;
-
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.times;
-
-import com.esprit.examen.entities.Operateur;
-import com.esprit.examen.repositories.OperateurRepository;
-import com.esprit.examen.services.IOperateurService;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.times;
+import javax.persistence.OneToMany;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
